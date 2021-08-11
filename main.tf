813f5ea090a481ea93226a1d5d1bbc2351a2d3c3
Ximena <ximenandrea.ro@gmail.com>
2021-08-10 22:13:21 -0400

    testing tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+# provider digitalocean{
+#     token = ${var.do_token}
+# }
+
+data "digitalocean_account" "account_info" {}
+
+output "droplet_limit" {
+  value = data.digitalocean_account.account_info.droplet_limit
+}
