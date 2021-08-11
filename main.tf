53d3d903a7a22bac3e66cac0b1b18d01ead8235b
Ximena <ximenandrea.ro@gmail.com>
2021-08-11 15:25:04 -0400

    testing tf

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-terraform {
-    required_providers {
-        digitalocean = {
-            source  = "digitalocean/digitalocean"
-            version = "~> 2.0"
-    }
-  }
-}
-
-# provider digitalocean{
-#     token = ${var.do_token}
+# terraform {
+#     required_providers {
+#         digitalocean = {
+#             source  = "digitalocean/digitalocean"
+#             version = "~> 2.0"
+#     }
+#   }
 # }
+
+provider "digitalocean" {
+    token = "248610ca749fafda2605e566e1f2022d8d18815204cf1c69543585ba1fe997bc"
+}
 
 data "digitalocean_account" "account_info" {}
 
