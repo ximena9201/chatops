bc397fb61b6687194fcd5315518a809914b45898
Ximena <ximenandrea.ro@gmail.com>
2021-09-07 17:05:22 -0400

    Adding datasources

--- mainSetup.tf
+++ mainSetup.tf
@@ -13,7 +13,7 @@ module "databaseCluster" {
 
 module "database" {
   source          = "./modules/database"
-  cluster_name    = data.cluster_name
+  cluster_name    = data.digitalocean_database_cluster.cluster_name
   cluster_engine  = "pg"
   cluster_region  = "sfo2"
   cluster_size    = "db-s-1vcpu-1gb"
