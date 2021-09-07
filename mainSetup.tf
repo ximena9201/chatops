c0d9b27873fee8c6a62b73ea9861c031e03e9c64
Ximena <ximenandrea.ro@gmail.com>
2021-09-07 17:44:04 -0400

    Adding ximedb

--- mainSetup.tf
+++ mainSetup.tf
@@ -6,23 +6,23 @@ module "databaseCluster" {
   cluster_size    = "db-s-1vcpu-1gb"
   cluster_version = "11"
   cluster_nodecount  = 1
-  database_name   = random_pet.name.id
+  database_name   = "ximedb"
   do_token        = var.do_token
   project_name    = "Test"
 }
 
-module "database" {
-  source          = "./modules/database"
-  cluster_name    = "ximena-cluster"
-  cluster_engine  = "pg"
-  cluster_region  = "sfo2"
-  cluster_size    = "db-s-1vcpu-1gb"
-  cluster_version = "11"
-  cluster_nodecount  = 1
-  database_name   = "ximedbexample"
-  do_token        = var.do_token
-  # project_name    = "Test"
-}
+# module "database" {
+#   source          = "./modules/database"
+#   cluster_name    = "ximena-cluster"
+#   cluster_engine  = "pg"
+#   cluster_region  = "sfo2"
+#   cluster_size    = "db-s-1vcpu-1gb"
+#   cluster_version = "11"
+#   cluster_nodecount  = 1
+#   database_name   = "ximedbexample"
+#   do_token        = var.do_token
+#   # project_name    = "Test"
+# }
 
 
 resource "random_pet" "name" {
