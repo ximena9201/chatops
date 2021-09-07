515b88b45672b27a73df485e8105cb38cc4d3286
Ximena <ximenandrea.ro@gmail.com>
2021-09-07 18:21:51 -0400

    Fix db module

--- mainSetup.tf
+++ mainSetup.tf
@@ -1,15 +1,15 @@
-# module "databaseCluster" {
-#   source          = "./modules/databaseCluster"
-#   cluster_name    = "cluster1"
-#   cluster_engine  = "pg"
-#   cluster_region  = "sfo2"
-#   cluster_size    = "db-s-1vcpu-1gb"
-#   cluster_version = "11"
-#   cluster_nodecount  = 1
-#   database_name   = "ximedb2"
-#   do_token        = var.do_token
-#   project_name    = "Test"
-# }
+module "databaseCluster" {
+  source          = "./modules/databaseCluster"
+  cluster_name    = "cluster1"
+  cluster_engine  = "pg"
+  cluster_region  = "sfo2"
+  cluster_size    = "db-s-1vcpu-1gb"
+  cluster_version = "11"
+  cluster_nodecount  = 1
+  database_name   = "ximedb2"
+  do_token        = var.do_token
+  project_name    = "Test"
+}
 
 module "database" {
   source          = "./modules/database"
