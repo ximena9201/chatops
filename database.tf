bed35a22adf4a3bf2742fc0d6c441ca9350557a9
Ximena <ximenandrea.ro@gmail.com>
2021-09-07 16:08:16 -0400

    Adding terraform clusterdb 2

--- database.tf
+++ database.tf
@@ -1,6 +1,6 @@
 module "database" {
   source          = "./modules/database"
-  cluster_name    = "xime-cluster"
+  cluster_name    = "xime-cluster2"
   cluster_engine  = "pg"
   cluster_region  = "sfo2"
   cluster_size    = "db-s-1vcpu-1gb"
