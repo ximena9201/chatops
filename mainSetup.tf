249cb223094de654981c98ba6f8765be976d0614
Ximena <ximenandrea.ro@gmail.com>
2021-09-07 18:20:54 -0400

    Fix db module

--- mainSetup.tf
+++ mainSetup.tf
@@ -13,13 +13,12 @@
 
 module "database" {
   source          = "./modules/database"
-  cluster_id      =  data.digitalocean_database_cluster.cluster.id
-  # cluster_name    = "cluster1"
-  # cluster_engine  = "pg"
-  # cluster_region  = "sfo2"
-  # cluster_size    = "db-s-1vcpu-1gb"
-  # cluster_version = "11"
-  # cluster_nodecount  = 1
+  cluster_name    = module.databaseCluster.cluster_name
+  cluster_engine  = module.databaseCluster.cluster_engine
+  cluster_region  = module.databaseCluster.cluster_region
+  cluster_size    = module.databaseCluster.cluster_size
+  cluster_version = module.databaseCluster.cluster_version
+  cluster_nodecount  = module.databaseCluster.cluster_nodecount
   database_name   = "ximedbexample"
   do_token        = var.do_token
   project_name    = "Test"
