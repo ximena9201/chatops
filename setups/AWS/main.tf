a0d3eafbb127e116d02e71af24b25bc0b76e6dca
Ximena <ximenandrea.ro@gmail.com>
2021-10-05 17:40:48 -0400

    adding another bucket

--- setups/AWS/main.tf
+++ setups/AWS/main.tf
@@ -1,7 +1,7 @@
 
 module "AWS_S3Bucket" {
   source          = "../../modules/AWS"
-  bucket_name     = "ximenademo11"
+  bucket_name     = "ximenademo1122"
   AWS_ACCESS_KEY_ID = var.AWS_ACCESS_KEY_ID
   AWS_SECRET_ACCESS_KEY = var.AWS_SECRET_ACCESS_KEY
 
