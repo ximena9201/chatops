6977500505d93b081512e148bb59b9324a0a8d9a
Ximena <ximenandrea.ro@gmail.com>
2021-10-11 23:12:54 -0400

    adding new yaml config

--- setup/digitalOcean/main.tf
+++ setup/digitalOcean/main.tf
@@ -6,7 +6,7 @@ module "databaseCluster" {
   cluster_size    = "db-s-1vcpu-1gb"
   cluster_version = "11"
   cluster_nodecount  = 1
-  database_name   = "ximedb4"
+  database_name   = "devdb4"
   do_token        = var.do_token
   project_name    = "Test"
 }
