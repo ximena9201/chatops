9114fc0d59640808ec22bd5e446f3cf2624340ad
Ximena <ximenandrea.ro@gmail.com>
2021-10-11 21:38:20 -0400

    adding vars and s3 backend

--- modules/AWS/variables.tf
+++ modules/AWS/variables.tf
@@ -5,17 +5,6 @@
 # variable "AWS_SECRET_ACCESS_KEY" {
 #     type        = string
 #     description = "Secret Key"}
-
-variable "tagName" {
-    type        = string
-    description = "Tag Name"
-}
-
-variable "tagEnv" {
-    type        = string
-    description = " Tag Environment"
-}
-
 variable "bucket" {
     type        = string
     description = "Bucket Name"
